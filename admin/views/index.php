e3fe82cab87896e9856a25e133dc2f7d53435029
Akmal Najib <akmlnajib13@gmail.com>
2025-03-05 03:42:11 +0700

    add favicon

--- admin/views/index.php
+++ admin/views/index.php
@@ -34,21 +34,37 @@ if (isset($_GET['pesan']) && $_GET['pesan'] == 'berhasil') {
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
 	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+	<link rel="apple-touch-icon" sizes="57x57" href="<?= base_url('assets/fav/apple-icon-57x57.png') ?>">
+	<link rel="apple-touch-icon" sizes="60x60" href="<?= base_url('assets/fav/apple-icon-60x60.png') ?>">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?= base_url('assets/fav/apple-icon-72x72.png') ?>">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?= base_url('assets/fav/apple-icon-76x76.png') ?>">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?= base_url('assets/fav/apple-icon-114x114.png') ?>">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?= base_url('assets/fav/apple-icon-120x120.png ') ?>">
+	<link rel="apple-touch-icon" sizes="144x144" href="<?= base_url('assets/fav/apple-icon-144x144.png ') ?>">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?= base_url('assets/fav/apple-icon-152x152.png ') ?>">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('assets/fav/apple-icon-180x180.png ') ?>">
+	<link rel="icon" type="image/png" sizes="192x192" href="<?= base_url('assets/favandroid-icon-192x192.png') ?>">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?= base_url('assets/fav/favicon-32x32.png') ?>">
+	<link rel="icon" type="image/png" sizes="96x96" href="<?= base_url('assets/fav/favicon-96x96.png') ?>">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('assets/fav/favicon-16x16.png') ?>">
+	<link rel="manifest" href="/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
 	<title>Absensi Karyawan - Geolocation</title>
 	<!-- CSS files -->
 	<link href="<?= base_url('/assets/css/tabler.min.css?1738096685') ?>" rel="stylesheet" />
 	<link href="<?= base_url('assets/css/tabler-vendors.min.css?1738096685') ?>" rel="stylesheet" />
 	<link href="<?= base_url('assets/css/demo.min.css?1738096685') ?>" rel="stylesheet" />
 	<style>
-		
 		@import url('https://rsms.me/inter/inter.css');
 	</style>
 </head>
 
 <body onload="hide_loading();">
-<div class="loading overpower">
-<div class="loader"></div> 
-</div>
+	<div class="loading overpower">
+		<div class="loader"></div>
+	</div>
 
 	<script src="<?= base_url('assets/js/demo-theme.min.js?1738096685') ?>"></script>
 	<div class="page">
@@ -66,7 +82,7 @@ if (isset($_GET['pesan']) && $_GET['pesan'] == 'berhasil') {
 		</div>
 	</div>
 	<!-- Libs JS -->
-	 <script src="script.js"></script>
+	<script src="script.js"></script>
 	<script src="<?= base_url('assets/libs/apexcharts/dist/apexcharts.min.js?1738096685') ?>" defer></script>
 	<script src="<?= base_url('assets/libs/jsvectormap/dist/jsvectormap.min.js?1738096685') ?>" defer></script>
 	<script src="<?= base_url('assets/libs/jsvectormap/dist/maps/world.js?1738096685') ?>" defer></script>
@@ -75,9 +91,10 @@ if (isset($_GET['pesan']) && $_GET['pesan'] == 'berhasil') {
 	<script src="<?= base_url('assets/js/tabler.min.js?1738096685') ?>" defer></script>
 	<script src="<?= base_url('assets/js/demo.min.js?1738096685') ?>" defer></script>
 	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-	<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.7.1.js"
+		integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 	<?php
-		include "./layouts/script.php";
+	include "./layouts/script.php";
 	?>
 </body>
 
--- pegawai/views/index.php
+++ pegawai/views/index.php
@@ -35,6 +35,19 @@ if (isset($_GET['pesan']) && $_GET['pesan'] == 'berhasil') {
 	<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
 	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
 	<title>Absensi Karyawan - Geolocation</title>
+	<link rel="apple-touch-icon" sizes="57x57" href="<?= base_url('assets/fav/apple-icon-57x57.png') ?>">
+	<link rel="apple-touch-icon" sizes="60x60" href="<?= base_url('assets/fav/apple-icon-60x60.png') ?>">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?= base_url('assets/fav/apple-icon-72x72.png') ?>">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?= base_url('assets/fav/apple-icon-76x76.png') ?>">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?= base_url('assets/fav/apple-icon-114x114.png') ?>">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?= base_url('assets/fav/apple-icon-120x120.png ') ?>">
+	<link rel="apple-touch-icon" sizes="144x144" href="<?= base_url('assets/fav/apple-icon-144x144.png ') ?>">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?= base_url('assets/fav/apple-icon-152x152.png ') ?>">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('assets/fav/apple-icon-180x180.png ') ?>">
+	<link rel="icon" type="image/png" sizes="192x192" href="<?= base_url('assets/favandroid-icon-192x192.png') ?>">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?= base_url('assets/fav/favicon-32x32.png') ?>">
+	<link rel="icon" type="image/png" sizes="96x96" href="<?= base_url('assets/fav/favicon-96x96.png') ?>">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('assets/fav/favicon-16x16.png') ?>">
 	<!-- CSS files -->
 	<link href="<?= base_url('/assets/css/tabler.min.css?1738096685') ?>" rel="stylesheet" />
 	<link href="<?= base_url('assets/css/tabler-vendors.min.css?1738096685') ?>" rel="stylesheet" />
